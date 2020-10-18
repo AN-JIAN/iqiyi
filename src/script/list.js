23154fa5c572d0fbd823b1d46a3a4efec98fd4c1
AN-JIAN <657936744@qq.com>
2020-10-18 21:55:43 +0800

    第二次提交列表页数据

--- src/script/list.js
+++ src/script/list.js
@@ -83,6 +83,10 @@
                 });
                 $strhtml += '</ul>';
                 $list.html($strhtml);
+                // 添加懒加载
+                $(function() {
+                    $("img.lazy").lazyload({ effect: "fadeIn" });
+                });
                 //将页面中的li元素加载到两个数组中
                 $('.list li').each(function(index, element) {
                     array[index] = $(this);
@@ -91,51 +95,57 @@
             });
         }
     });
+
+
     // 3.排序，排序前的数组已经具有li元素
     $('.sort button').eq(0).on('click', function() {
         $.each(array_default, function(index, value) {
             $('.list ul').append(value);
             return;
         });
-        $('.sort button').eq(1).on('click', function() {
-            for (let i = 0; i < array.length - 1; i++) {
-                for (let j = 0; j < array.length - i - 1; j++) {
-                    prev = parseFloat(array[j].find('.price').html().substring(1)); //截取上一个价格
-                    next = parseFloat(array[j + 1].find('.price').html().substring(1)); //截取下一个价格
-                    //通过价格判断，改变数组li的位置
-                    if (prev > next) {
-                        let temp = array[j];
-                        array[j] = array[j + 1];
-                        array[j + 1] = temp;
-                    }
+    });
+
+
+    //升序
+    $('.sort button').eq(1).on('click', function() {
+        for (let i = 0; i < array.length - 1; i++) {
+            for (let j = 0; j < array.length - i - 1; j++) {
+                prev = parseFloat(array[j].find('.price').html().substring(1)); //截取上一个价格
+                next = parseFloat(array[j + 1].find('.price').html().substring(1)); //截取下一个价格
+                //通过价格判断，改变数组li的位置
+                if (prev > next) {
+                    let temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
                 }
             }
-            $('.list ul').empty();
-            $.each(array, function(index, value) {
-                console.log(value);
-                $('.list ul').append(value);
-            })
-        })
-        $('.sort button').eq(2).on('click', function() {
-            for (let i = 0; i < array.length - 1; i++) {
-                for (let j = 0; j < array.length - i - 1; j++) {
-                    prev = parseFloat(array[j].find('.price').html().substring(1)); //截取上一个价格
-                    next = parseFloat(array[j + 1].find('.price').html().substring(1)); //截取下一个价格
-                    //通过价格判断，改变数组li的位置
-                    if (prev > next) {
-                        let temp = array[j];
-                        array[j] = array[j + 1];
-                        array[j + 1] = temp;
-                    }
-                }
-            }
-            $.each(array, function(index, value) {
-                console.log(value);
-                $('.list ul').append(value);
-            })
-        })
+        };
         $.each(array, function(index, value) {
             $('.list ul').append(value);
         })
-    })
+
+
+    });
+
+    // 降序
+
+    $('.sort button').eq(2).on('click', function() {
+        for (let i = 0; i < array.length - 1; i++) {
+            for (let j = 0; j < array.length - i - 1; j++) {
+                prev = parseFloat(array[j].find('.price').html().substring(1)); //截取上一个价格
+                next = parseFloat(array[j + 1].find('.price').html().substring(1)); //截取下一个价格
+                //通过价格判断，改变数组li的位置
+                if (prev < next) {
+                    let temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                }
+            }
+        }
+        $.each(array, function(index, value) {
+            $('.list ul').append(value);
+        })
+    });
+
+
 }(jQuery)
